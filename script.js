73b44bc89544be575a8a3c36ffb04014f34679cf
Smoopfrog <jeffstinson@hotmail.com>
2023-04-13 21:56:25 -0700

    change component display on submit

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const btns = document.querySelectorAll("btn");
+// const btns = document.getElementsByClassName("btn");
+// console.log
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     btns.forEach((f) => f.classList.remove("active"));
+//     e.target.classList.toggle("active");
+//   });
+// });
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    btns.forEach((f) => f.classList.remove("active"));
-    e.target.classList.toggle("active");
-  });
-});
+// const button = document.getElementById("button");
+// console.log(button)
+// const buttonPressed = (event) => {
+//   console.log("hello");
+//   // e.target.classList.toggle("active");
+// };
+
+// button.addEventListener("click", console.log('hiya'));
 
 const submitRating = (event) => {
   event.preventDefault();
+
+  const ratingComponent = document.getElementsByClassName("rating")[0];
+  ratingComponent.style.display = "none";
+  const thanksComponent = document.getElementsByClassName("thanks")[0];
+  thanksComponent.style.display = "flex";
 };
